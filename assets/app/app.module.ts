fd58bd8862bcf5620b4fee772b0a64d20a5ff34f
Berrydoo <Jim.dewberry@Gmail.com>
2016-11-22 11:34:10 -0500

    routing and front end improvements

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -1,13 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
-import { AppComponent } from "./app.component";
+import { AppComponent } from './app.component';
+import { MessageInputComponent} from './messages/message-input.component';
+import { MessageListComponent } from './messages/message-list.component';
+import { MessageComponent } from './messages/message.component';
+import { MessagesComponent } from './messages/messages.component';
+import { AuthenticationComponent} from './auth/authentication.component';
+import { HeaderComponent } from './header.component';
+import { routing } from './app.routing';
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        MessageListComponent,
+        MessageComponent,
+        MessageInputComponent,
+        MessagesComponent,
+        AuthenticationComponent,
+        HeaderComponent
     ],
-    imports: [BrowserModule],
+    imports: [BrowserModule, FormsModule, routing],
     bootstrap: [AppComponent]
 })
 export class AppModule {
--- routes/app.js
+++ routes/app.js
@@ -4,30 +4,7 @@ var User = require('../models/user');
 
 
 router.get('/', function(req, res, next) {
-
-    User.findOne({}, (err, doc) => {
-        if (err) {
-            return res.send('Error!');
-        }
-
-        res.render('node', {
-            email: doc.email
-        });
-    });
-
-});
-
-router.post('/', function(req, res, next) {
-    var email = req.body.email;
-    var user = new User({
-        firstName: 'Jim',
-        lastName: 'Dewberry',
-        password: 'super-secret',
-        email
-    });
-
-    user.save();
-    res.redirect('/');
+    res.render('index');
 });
 
 module.exports = router;
