1f65dd1de535bf12cd4f9591be94f159ba5290e0
Berrydoo <Jim.dewberry@Gmail.com>
2016-11-22 15:05:13 -0500

    finished the front end mostly

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { MessageInputComponent} from './messages/message-input.component';
@@ -28,7 +28,7 @@ import { SignupComponent } from './auth/signup.component';
         SigninComponent,
         SignupComponent
     ],
-    imports: [BrowserModule, FormsModule, routing],
+    imports: [BrowserModule, FormsModule, routing, ReactiveFormsModule],
     bootstrap: [AppComponent]
 })
 export class AppModule {
