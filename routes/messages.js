55e69655a4ebe6bfb932ae4f0b2cc16d8fecd994
Berrydoo <Jim.dewberry@Gmail.com>
2016-11-23 09:50:04 -0500

    save and retrieve messages from mongo for display

--- routes/messages.js
+++ routes/messages.js
@@ -23,4 +23,21 @@ router.post('/', function(req, res, next) {
     });
 });
 
+router.get('/', function( req, res, next){
+    Message.find({})
+      .exec( function(err, messages){
+        if(err){
+          return res.status(500).json({
+            title:'An error occurred',
+            error: err
+          });
+        }
+
+        res.status(200).json({
+          message: 'Success',
+          obj: messages
+        });
+      });
+});
+
 module.exports = router;
